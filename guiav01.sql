4d1349008738a80f9bfb5e8d868e9c6cd83db175
acsasilveira <acsa.silveiras@gmail.com>
2024-11-08 17:47:29 -0300

    .

--- guiav01.sql
+++ guiav01.sql
@@ -1,27 +1,27 @@
 -- Comando que cria o banco de dados
-/* create database guia01; */
+    create database guia01; 
 
  -- Indicar o banco de dados que será usado
- /* use guia01; */
+    use guia01; 
 
 -- Criação da tabela TbPessoa
-/* create table TbPessoa
+    create table TbPessoa
 (	id int,
     nome varchar(255),
     CPF varchar(14),
     primary key(id)
-)engine=InnoDB;*/
+)engine=InnoDB;
 
 -- Inserção de dados na tabela TbPessoa
 
-/*insert into TbPessoa(id, nome, CPF) values
+    insert into TbPessoa(id, nome, CPF) values
 (1, "Acsa", "123.456.789-10"),
 (2, "Calebe", "123.456.789-10"),
 (3, "Gabriela", "123.456.789-10"),
-(4, "Vicente", "123.456.789-10");*/
+(4, "Vicente", "123.456.789-10");
 
 -- Tornar algum atributo único
-/* create UNIQUE_INDEX cpf_unico on TbPessoa(CPF) */
+    create UNIQUE_INDEX cpf_unico on TbPessoa(CPF) 
 
 -- Comando para apagar o banco de dados
-/* DROP database guia01;*/
+    DROP database guia01;
